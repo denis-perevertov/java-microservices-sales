4966e57f623ab93424d0d623a6ccd712a5093153
Denis Perevertov <arrow8086527denis@gmail.com>
2024-03-05 05:10:14 -0500

    more files

---
+++ api-gateway/src/main/java/com/dp/apigateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.dp.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("payment_service_route", r -> r
+                        .path("/api/payment/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri("lb://payment-service"))
+                .route("product_service_route", r -> r
+                        .path("/api/product/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri("lb://product-service"))
+                .route("discovery_service_route", r -> r
+                        .path("/eureka/web")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://localhost:8761"))
+                .route("discovery_service_resources_route", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
---
+++ eureka-server/src/test/java/com/dp/paymentservice/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
